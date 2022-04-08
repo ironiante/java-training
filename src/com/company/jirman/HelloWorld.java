c1e2209f68be8d702a27b46602e2866bf1e26b8a
jirman rodriguez <87489149+ironiante@users.noreply.github.com>
2022-04-07 20:36:02 -0500

    Initial commit

---
+++ src/com/company/jirman/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.company.jirman;
+
+public class HelloWorld {
+ /* public: modificador de acceso
+ static: es de la clase helloworld
+ void: tipo de retorno no devuelve nada
+  main: metodo principal
+  (String[] args) : array de argumentos  */
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+    }
+}
