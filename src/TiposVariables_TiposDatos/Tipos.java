79a66c4efbb75fe8159a291d003102adc6a4f730
jirman rodriguez <87489149+ironiante@users.noreply.github.com>
2022-04-07 21:53:11 -0500

    variables y tipos de datos.

---
+++ src/TiposVariables_TiposDatos/Tipos.java
@@ -0,0 +1,37 @@
+package TiposVariables_TiposDatos;
+
+public class Tipos {
+/**
+ * @author jirman
+   */
+public static void main(String[] args) {
+    // como declarar variables:
+    // tipo identificador = 30;
+    //tipo identificador;
+    //identificador=30;
+//_____________________________________________________________________
+// enteros
+byte number1=1; // 1 byte
+short number2=2; // 2 byte
+int number3=3;   // 4 bytes
+long number4=4;  // 8 bytes
+//____________________________________________________________________
+// ---> punto flotante
+float decimal1=4.9f;
+double decimal2=9.999d;
+//______________________________________________________________________
+//---> caracter
+char caracter1='a';
+//______________________________________________________________________
+//---->booleanos
+    boolean verdadero=true;
+    boolean falso = false;
+// cadena de texto
+String nombre ="jirman";
+String apellido ="rodriguez";
+//__________________________________________________________________
+//---> tipos envoltorios
+Integer numero=null;
+Long numero2=2L;
+}
+}
