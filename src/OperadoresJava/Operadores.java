e4707082f739f27af82fc42596ff668cde85dbf5
jirman rodriguez <87489149+ironiante@users.noreply.github.com>
2022-04-09 20:37:13 -0500

    operadores

---
+++ src/OperadoresJava/Operadores.java
@@ -0,0 +1,40 @@
+package OperadoresJava;
+
+public class Operadores {
+/**
+ * Tipos de operadores
+ * - Sobrescribir el valor de una variable
+ * - Operaciones con decimales
+ * - Operadores lógicos
+ * - Operadores de asignación
+ * - Operadores de incremento y decremento
+ * - Concatenación
+
+  */
+public static void main(String[] args) {
+/**
+* tipos de operadores:
+ - aritmeticos ---> +-* %
+ ____________________________________________________________________
+
+*/
+int number1=5;
+int number2=2;
+
+int resultado1=number1+number2;
+int resultado2=number1-number2;
+int resultado3=number1/number2;
+int resultado4=number1* number2;
+
+//_________________________________________________________________________
+// logicos,relacion,comparacion,booleanos
+/**
+ * > , >=,<,<==,==(comparacion);!=(distinto),&&(and),(or), !true(negacion)
+ * _____________________________________________________________________
+  */
+// operadores de asignacion
+    // =,+=,-=;/=,*=,%=
+//incremento (se utiliza en ciclos for) ---> ++
+    // decremento ---> --
+}
+}
