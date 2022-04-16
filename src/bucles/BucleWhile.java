992d023e90b8118a090f0f586a103fef1788ec7d
jirman rodriguez <87489149+ironiante@users.noreply.github.com>
2022-04-16 12:14:21 -0500

    BucleWhile

---
+++ src/bucles/BucleWhile.java
@@ -0,0 +1,22 @@
+package bucles;
+
+/**
+ * while loop
+ * Qué es un bucle while y para que se usa.
+ *  Nueva clase y método “main”
+ *  Estructura del bucle “while”
+ *  Bucles infinitos y condición de salida
+ *  Comprobar bucle infinito
+ */
+public class BucleWhile {
+    public static void main(String[] args) {
+// se evalua una condiciones  y metodos, se usa contadores
+
+     int count =0;
+     while (count<10){
+         System.out.println("hola mundo  "+count);
+         count++;
+     }
+        System.out.println("fin");
+    }
+}
