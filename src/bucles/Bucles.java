dc5b9aec95f0a04d1bdddd838b8d7ad6692a50ce
jirman rodriguez <87489149+ironiante@users.noreply.github.com>
2022-04-16 11:39:15 -0500

    Bucles

---
+++ src/bucles/Bucles.java
@@ -0,0 +1,24 @@
+package bucles;
+
+/**
+ * foorloop
+ * Qué son los bucles for y como utilizarlos.
+ *  Nueva clase y método “main”
+ *  Sintaxis del bucle “for”
+ *  Imprimir un texto varias veces
+ *  Cambio de operador
+ *  Depuración de código
+ *  Cuándo usarlos
+ *  Alterar el bucle
+ */
+public class Bucles {
+    public static void main(String[] args) {
+// condicion de comienzo donde esta i y comienza en cero
+        //condicion de parada
+        // condicion de paso, como se incrementa ese bucle
+        // vamos a imprimir un texto 10 veces
+        for (int i = 0; i < 10; i++) {
+            System.out.println( i  +   "hola mundo");
+        }
+    }
+}
